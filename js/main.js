32a4967193753aa571a3ed6fbe070a82349b5a3d
willy-vvu <willy-vvu@gmail.com>
2013-03-18 16:39:41 -0400

    First commit.

---
+++ js/main.js
@@ -0,0 +1,34 @@
+function MainCtrl($scope){
+	$scope.socket=io.connect()//Used for transferring information efficiently
+	$scope.location=location//The current url
+	$scope.responded=true//Whether or not the person has responded, assume they have until otherwise
+	$scope.respond=function(){//Sends the data off to the server
+		this.responded=true
+		this.socket.emit('responding',this.response)//Send your response to the server
+	}
+	$scope.socket.on('reply',function(entry){//When the server replies with the assignment entry in the db
+		$scope.$apply(function(){
+			if(entry===false){//No such assignment. Sorry.
+				$scope.responded=false
+				$scope.response=''
+				//$scope.location.hash=''
+			}
+			else{
+				$scope.location.hash=entry._id
+				$scope.response=entry.response
+			}
+		})
+	})
+	//Check to see if they are getting a previous one by hash
+	$scope.init=function(){
+		if($scope.location.hash.length){
+			$scope.responded=true
+			$scope.socket.emit('init',$scope.location.hash.slice(1))
+		}
+		else{
+			$scope.responded=false
+		}
+	}
+	$scope.init()
+	window.addEventListener('hashchange',$scope.init)
+}
---
+++ server.js
@@ -0,0 +1,56 @@
+var app = require('http').createServer(handler)
+,io=require('socket.io').listen(app)
+,fs=require('fs')
+,path=require('path')
+,url=require('url'),
+mime=require('mime'),
+mongo=require('mongojs'),
+db=mongo.connect('PRIMES',['assignments'])
+
+app.listen(process.env.PORT||5000);
+function handler (req, res) {
+	var parsed=url.parse(req.url)
+	var uri=parsed.pathname
+	var query=parsed.query
+	uri=uri.replace(/%20/g,' ')
+	console.log(uri)
+	if(uri=='/'){
+			uri='/main.html'
+	}
+	var filename = path.join(process.cwd(), uri);
+	fs.readFile(filename,
+		function (err, data) {
+		if (err) {
+			res.writeHead(404);
+			return res.end('Error loading file...');			  
+		}
+		res.setHeader('Content-type',mime.lookup(uri));
+		res.writeHead(200);
+		res.end(data);
+	})
+}
+io.on('connection',function(socket){
+	socket.on('init',function(id){//When someone tries to get an existing assignment
+		//Find it for them
+		try{
+			mongoId=mongo.ObjectId(id)
+		}
+		catch(e){//Id is invalid :(
+			socket.emit('reply',false)
+			return
+		}
+		db.assignments.find({_id:mongoId},function(e,r){
+			if(r.length){
+				socket.emit('reply',r[0])
+			}
+			else{
+				socket.emit('reply',false)
+			}
+		})
+	}).on('responding',function(response){//When someone submits a response
+		//Put it in the database and give 'em a way to get back to it
+		db.assignments.save({response:response},function(e,r){
+			socket.emit('reply',r)
+		})
+	})
+})
