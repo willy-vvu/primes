1c3f932968b79c252894374f89d4af660c9e57d4
willy-vvu <willy.vvu@gmail.com>
2013-08-03 21:02:52 -0400

    Added down button and touched up styling

--- js/main.js
+++ js/main.js
@@ -1,46 +1,50 @@
 function MainCtrl($scope) {
-	/*io.configure(function() {
-	 io.set("transports", ["xhr-polling"]);
-	 io.set("polling duration", 10);
-	 });
-	 io.connect();*/
-	$scope.state = {
-		loggedin : false,
-		selectedclass : 0, //-1
-		selectedassignment : -1
-	};
-	$scope.user = {
-		username : "",
-		password : "",
-		data : [{
-			name : "Calculus",
-			assignments : [{
-				name : "4-3 Handout"
-			}, {
-				name : "4-3 Handout"
-			}, {
-				name : "4-3 Handout"
-			}, {
-				name : "4-3 Handout"
-			}, {
-				name : "4-3 Handout"
-			}, {
-				name : "4-3 Handout"
-			}, {
-				name : "4-3 Handout"
-			}, {
-				name : "4-3 Homework"
-			}]
-		}, {
-			name : "Physics",
-			assignments : [{
-				name : "2-1 Review"
-			}, {
-				name : "2-1 Handout"
-			}]
-		}]
-	};
-	$scope.login = function() {
-		this.state.loggedin = true
-	};
+  /*io.configure(function() {
+   io.set("transports", ["xhr-polling"]);
+   io.set("polling duration", 10);
+   });
+   io.connect();*/
+  $scope.state = {
+    loggedin : false,
+    selectedclass : 0, //-1
+    selectedassignment : -1,
+    menuvisible : false
+  };
+  $scope.user = {
+    username : "",
+    password : "",
+    data : [{
+      name : "Calculus",
+      assignments : [{
+        name : "4-3 Handout"
+      }, {
+        name : "4-3 Handout"
+      }, {
+        name : "4-3 Handout"
+      }, {
+        name : "4-3 Handout"
+      }, {
+        name : "4-3 Handout"
+      }, {
+        name : "4-3 Handout"
+      }, {
+        name : "4-3 Handout"
+      }, {
+        name : "4-3 Homework"
+      }]
+    }, {
+      name : "Physics",
+      assignments : [{
+        name : "2-1 Review"
+      }, {
+        name : "2-1 Handout"
+      }]
+    }]
+  };
+  $scope.login = function() {
+    this.state.loggedin = true
+  };
+  $scope.logout = function() {
+    this.state.loggedin = false
+  };
 }
